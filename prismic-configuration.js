e977aad932aa42b02a1f96546642c1241f295e7e
Anastasia Mikheichyk <navstyachka@gmail.com>
2019-07-26 11:00:51 +0200

    initial

---
+++ prismic-configuration.js
@@ -0,0 +1,4 @@
+module.exports = {
+  apiEndpoint: 'https://hyam-sample-website.prismic.io/graphql',
+  accessToken: 'MC5YVG1Fa3hFQUFDSUE2RTZq.dVwj77-9N0Um77-9Te-_vU7vv73vv70OQV_vv71m77-9JkhxMVLvv71nUkBjJDTvv70',
+};
